af8cc29128192eb7c04d172097fd704b852f0813
Misho Rostomashvili <Rostomashvili05@gmail.com>
2023-01-20 17:32:00 +0400

    Add function to check if input is correct

--- main.cpp
+++ main.cpp
@@ -5,6 +5,12 @@
 #include <regex>
 using namespace std;
 
+// Function to check if input is Roman numeral
+bool check_roman(string input) {
+    regex reg("^C{0,1}(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$");
+    return regex_match(input, reg);
+}
+
 int main(){
 
   return 0;
