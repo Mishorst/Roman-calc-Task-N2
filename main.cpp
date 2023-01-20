edaac1e2c746470e1b3cb0d5d37b88ffe43a5513
tatandashvili <tatandashvili@gmail.com>
2023-01-20 14:34:09 +0100

    new function

--- main.cpp
+++ main.cpp
@@ -11,6 +11,23 @@ bool check_roman(string input) {
     return regex_match(input, reg);
 }
 
+// Function to convert a Roman numeral string to an integer
+int romanToInt(string s, map<char, int> &roman) {
+    int result = 0;
+    for (int i = 0; i < s.size(); i++) {
+        
+        // Add the value of the current Roman numeral
+        result += roman[s[i]];
+        
+        // Check if the current Roman numeral has a lower value than the previous one
+        if (i > 0 && roman[s[i]] > roman[s[i - 1]])
+        
+            // Subtract the value of the previous Roman numeral twice as it has been added in the previous step
+            result -= 2 * roman[s[i - 1]];
+    }
+    return result;
+}
+
 int main(){
 
   return 0;
